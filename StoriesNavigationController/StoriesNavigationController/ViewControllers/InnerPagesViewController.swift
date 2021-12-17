9f8a99cb374a42f1a6f74ebbfed69c4d8ed7aa9d
Gabo-Segura <gabrielsegurao@me.com>
2021-12-16 20:04:23 -0500

    Finnish Assignment 3 iOS

---
+++ StoriesNavigationController/StoriesNavigationController/ViewControllers/InnerPagesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CoatRoomControllerViewController.swift
+//  StoriesNavigationController
+//
+//  Created by Gabriel Eduardo on 12/16/21.
+//  Copyright © 2021 Gabriel Eduardo. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func toFisrtScreen() {
+    }
+}
+
+class InnerPagesViewController: FeedbackButtonViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
